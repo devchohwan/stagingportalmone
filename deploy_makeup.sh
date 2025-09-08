0caf925d4cc4f16b3b463f006d396c7cfb3ce7fe
Cho Sung Hwan <devchohwan@gmail.com>
2025-09-09 03:22:21 +0900

    Revert "Add deployment scripts for practice and makeup services"
    This reverts commit 47ec1c94b9c7693f6976daa72b776d5ace63daf8.

--- deploy_makeup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Deploy makeup service only
-
-echo "Deploying makeup service..."
-kamal accessory boot makeup
-echo "Makeup service deployment complete!"
--- deploy_practice.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Deploy practice service only
-
-echo "Deploying practice service..."
-kamal accessory boot practice
-echo "Practice service deployment complete!"
