cd8946190d01b8baa627b49e59804f20d2f9d6ce
Cho Sung Hwan <devchohwan@gmail.com>
2025-09-09 03:21:14 +0900

    Add deployment scripts for practice and makeup services

---
+++ deploy_makeup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Deploy makeup service only
+
+echo "Deploying makeup service..."
+kamal accessory boot makeup
+echo "Makeup service deployment complete!"
---
+++ deploy_practice.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Deploy practice service only
+
+echo "Deploying practice service..."
+kamal accessory boot practice
+echo "Practice service deployment complete!"
