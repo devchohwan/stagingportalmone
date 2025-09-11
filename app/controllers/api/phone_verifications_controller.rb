c534f9cf45b75bc0f47fb83dae92607a454b7e6b
Cho Sung Hwan <devchohwan@gmail.com>
2025-09-11 15:27:19 +0900

    Fix undefined callback error in phone verifications controller

--- app/controllers/api/phone_verifications_controller.rb
+++ app/controllers/api/phone_verifications_controller.rb
@@ -1,7 +1,7 @@
 class Api::PhoneVerificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  skip_before_action :require_login, if: -> { defined?(require_login) }
-  skip_before_action :authenticate_user!, if: -> { defined?(authenticate_user!) }
+  skip_before_action :require_login, raise: false
+  skip_before_action :authenticate_user!, raise: false
   skip_before_action :update_reservation_statuses
   
   # POST /api/send-verification
