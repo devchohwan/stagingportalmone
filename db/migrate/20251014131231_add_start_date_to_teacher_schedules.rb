d831273cab966378b8026dfdc4d89127f24643a7
Cho Sung Hwan <devchohwan@gmail.com>
2025-10-14 23:28:35 +0900

    🐛 마이그레이션 중복 컬럼 에러 수정 (column_exists 체크 추가)

--- db/migrate/20251014131231_add_start_date_to_teacher_schedules.rb
+++ db/migrate/20251014131231_add_start_date_to_teacher_schedules.rb
@@ -1,5 +1,5 @@
 class AddStartDateToTeacherSchedules < ActiveRecord::Migration[8.0]
   def change
-    add_column :teacher_schedules, :start_date, :date
+    add_column :teacher_schedules, :start_date, :date unless column_exists?(:teacher_schedules, :start_date)
   end
 end
