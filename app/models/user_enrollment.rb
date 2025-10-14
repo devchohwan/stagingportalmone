12f53f48090920afaadc171091e1056214957715
Cho Sung Hwan <devchohwan@gmail.com>
2025-10-14 13:43:34 +0900

    🐛 결제 캘린더 데이터베이스 컬럼명 수정 - user_enrollment_id → enrollment_id

--- app/models/user_enrollment.rb
+++ app/models/user_enrollment.rb
@@ -108,12 +108,13 @@ class UserEnrollment < ApplicationRecord
 
   # 다음 결제 예정일 계산
   def next_payment_date
-    # 이 수강권의 마지막 결제 정보
-    last_payment = Payment.where(user_enrollment_id: id)
+    # 이 수강권의 마지막 결제 정보 (enrollment_id 사용)
+    last_payment = Payment.where(enrollment_id: id)
                          .order(payment_date: :desc)
                          .first
 
     return nil unless last_payment
+    return nil unless last_payment.months.present?
 
     # 결제일 + 수강기간(개월)
     last_payment.payment_date + last_payment.months.months
